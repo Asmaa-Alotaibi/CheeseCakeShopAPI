204167f7f7e4451f327e1f2e9aa87b751de3d320
Asmaa-Alotaibi <asmaa-otaibi@yahoo.com>
2020-11-19 19:16:20 +0300

    backend Done !

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const items = require("./items");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+app.listen(8000, () => {
+  console.log("The application is running on localhost:8000");
+});
